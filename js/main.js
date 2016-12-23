0a6b52d693723a97e4ef9e4b051327f380df95f2
Cronos <lexsandr46@gmail.com>
2016-12-23 15:07:16 +0300

    Add js code in main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
     var x = document.getElementById("myTopnav");
+    var y = document.getElementById("first-section-content")
     if (x.className === "topnav") {
         x.className += " responsive";
+        y.className += " first-section-responsive";
     } else {
         x.className = "topnav";
+        y.className = "col-md-8";
     }
 }
